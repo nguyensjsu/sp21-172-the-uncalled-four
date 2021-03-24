5b27fc90f4e5a9d20964b4461905aa718fa71300
Guiller Dalit <guillerdalit@gmail.com>
2021-03-24 14:10:02 -0700

    Starbucks Initial Commit 7e1f9f6

--- starbucks/src/main/java/com/example/starbucks/StartbucksCommand.java
+++ starbucks/src/main/java/com/example/starbucks/StartbucksCommand.java
@@ -10,7 +10,6 @@ class StartbucksCommand {
   private String message;
   private String state;
   private String timestamp;
-  private String hash;
 
   public void setAction(String action) {
     this.action = action;
@@ -26,10 +25,6 @@ class StartbucksCommand {
 
   public void setTimestamp(String timestamp) {
     this.timestamp = timestamp;
-  }
-
-  public void setHash(String hash) {
-    this.hash = hash;
   }
 
   public String getAction() {
@@ -48,8 +43,5 @@ class StartbucksCommand {
     return this.timestamp;
   }
 
-  public String getHash() {
-    return this.hash;
-  }
 
 }
