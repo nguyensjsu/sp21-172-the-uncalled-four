c3848f5a0d89359353c0f68b64854f51bbb3c7aa
Guiller Dalit <guillerdalit@gmail.com>
2021-04-20 20:33:53 -0700

    Mysql

---
+++ mysql/starbucks-data.sql
@@ -0,0 +1,69 @@
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Caffe Americano', 3.45, 'Americano' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Blonde Roast', 3.45, 'BrewedCoffee' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Caffe Misto', 3.45, 'BrewedCoffee' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Featured Starbuck Dark Roast Coffee', 3.45, 'BrewedCoffee' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Pike Place Roast', 3.45, 'BrewedCoffee' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Decaf Pike Place Roast', 3.45, 'BrewedCoffee' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Capuccino', 3.45, 'Capuccino' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Espresso', 3.45, 'EspressoShots' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Espresson Con Panna', 3.45, 'EspressoShots' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Flat White', 3.45, 'FlatWhite' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Honey Almondmilk Flat White', 3.45, 'FlatWhite' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Pistachio Latte', 3.45, 'Latte' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Caffe Latte', 3.45, 'Latte' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Cinnamon Dolce Latte', 3.45, 'Latte' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Starbucks Reserve Latte', 3.45, 'Latte' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Starbucks Reserve Hazelnut Bianco Latte', 3.45, 'Latte' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Starbucks Blonde Vanilla Latte', 3.45, 'Latte' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Caramel Macchiato', 3.45, 'Macchiato' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Espresso Macchiato', 3.45, 'Macchiato' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Caffe Mocha', 3.45, 'Mocha' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('Starbucks Reserve Dark Chocolate Mocha', 3.45, 'Mocha' ) ;
+
+insert into drink ( drink_name, price,  drink_type ) 
+values	('White Chocolate Mocha', 3.45, 'Mocha' ) ;
+
+
+
