3f328d1c633b80ae64d9846ce52ab2b0a718cd10
Guiller Dalit <guillerdalit@gmail.com>
2021-05-11 20:05:59 -0700

    Initial Commit Backoffice

---
+++ backoffice/src/test/java/com/example/backofficerabbitmq/BackOfficeRabbitmq.java
@@ -0,0 +1,13 @@
+package com.example.backoffice.src.test.java.com.example.backofficerabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackOfficeRabbitmq {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
